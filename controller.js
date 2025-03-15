5ab6872bca56b13dff654cea84f17087687bcfa2
gilbertrendon <jilberlv@hotmail.com>
2025-03-15 14:45:45 -0500

    crud finiquitado

--- controller.js
+++ controller.js
@@ -5,7 +5,7 @@ app.use(cors());
 app.use(express.json())
 const {getProducts} = require('./mongoConection.js')
 const {postProduct,putProduct} = require('./mongoConection.js')
-const {getProductById} = require("./mongoConection");
+const {getProductById, delProduct} = require("./mongoConection");
 const router = express.Router();
 port = 3000;  // Puerto donde escuchará el servidor
 app.use('/', router);  // Usa el enrutador en la ruta raíz
@@ -53,42 +53,25 @@ app.use('/', router);  // Usa el enrutador en la ruta raíz
     router.put('/updp/:id', async (req, res) => {
         const id  = req.params.id;
         try {
-/*
-            producto_actualizar = getProductById(id)
-*/
-          /*  const uProduct = {
-                name: req.body.name,
-                description: req.body.description,
-                price: req.body.price,
-                stock: req.body.stock,
-                imageUrl: req.body.imageUrl,
-                createdAt: req.body.createdAt,
-                updatedAt: req.body.updatedAt,
-                basePrice: req.body.basePrice,
-                brand: req.body.brand,
-                inStock: req.body.inStock,
-                sku: req.body.sku,
-            };*/
             producto_actualizar = await putProduct(req, res,id)
-
-            /* producto_actualizar = putProduct(req.body, res)
-
-            const uProduct = {
-                name: req.body.name,
-                description: req.body.description,
-                price: req.body.price,
-                stock: req.body.stock,
-                category: req.body.category,
-            };
-            producto_actualizar = putProduct(req.body, res)
-
-            const savedProduct = await postProduct(req, res);*/ // Guardamos el producto en la base de datos.
             res.status(201).json({message: "Producto creado exitosamente"});
         } catch (error) {
             console.error("Error al crear el producto:", error);
             res.status(500).json({message: "Error al crear el producto"}); // Respuesta con error.
         }
     });
+    router.delete('/delet/:id', async (req, res) => {
+        const id  = req.params.id;
+        try {
+            producto_actualizar = await delProduct(id)
+            res.status(201).json({message: "Producto borrado exitosamente"});
+        } catch (error) {
+            console.error("Error al borrar el producto:", error);
+            res.status(500).json({message: "Error al borrar el producto"}); // Respuesta con error.
+        }
+    });
+
+
 }
 
 
